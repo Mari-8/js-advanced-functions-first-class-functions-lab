4c62d94fd83361537f5968f4d79c7d7f1285efe1
Mari-8 <ryanaltimari8@gmail.com>
2021-03-18 21:58:24 -0500

    commit tree 9ccdf2d9e33888ccbe48a983ca6518cea1551d40

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (function(drivers){
+    let firstDrivers = []
+    firstDrivers.push(drivers[0], drivers[1])
+    return firstDrivers
+})
+
+const returnLastTwoDrivers = (function(drivers){
+    let lastDrivers = [] 
+    lastDrivers.push(drivers.slice(-2)[0], drivers.slice(-1)[0])
+    return lastDrivers
+})
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multi) {
+    let fare = multi
+    return function(fare) {
+        const newFare = fare * 5
+        return newFare
+    }
+
+   
+
+    
+}
+
+function fareDoubler(multi) {
+    return multi * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, selected) {
+    if (selected == returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else {
+        return returnLastTwoDrivers(drivers) 
+    }
+}
+
+let drivers = ["Ryan", "Kins", "Blake", "Dev", "Stella"]
+
+function start() {
+    selectDifferentDrivers(drivers, selectingDrivers)
+}
